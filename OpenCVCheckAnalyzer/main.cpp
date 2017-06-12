97b668b8b28e9acba1647e7083eedfc94d2cdfde
Rishi Gupta <rishig@Rishis-Mac-Pro.local>
2017-06-11 21:13:57 -0700

    Two passes

--- OpenCVCheckAnalyzer/main.cpp
+++ OpenCVCheckAnalyzer/main.cpp
@@ -3,6 +3,8 @@
 #include <opencv2/highgui/highgui.hpp>
 #include "opencv/cvaux.hpp"
 
+#include <iostream>
+
 using namespace cv;
 
 void ExerciseFive()
@@ -276,6 +278,8 @@ void ExerciseTwelve()
 
 cv::Point2f computeIntersect(cv::Vec4i a, cv::Vec4i b)
 {
+    static int eps = 50;
+    
     int x1 = a[0], y1 = a[1], x2 = a[2], y2 = a[3];
     int x3 = b[0], y3 = b[1], x4 = b[2], y4 = b[3];
     if (float d = ((float)(x1-x2) * (y3-y4)) - ((y1-y2) * (x3-x4)))
@@ -284,10 +288,10 @@ cv::Point2f computeIntersect(cv::Vec4i a, cv::Vec4i b)
         pt.x = ((x1*y2 - y1*x2) * (x3-x4) - (x1-x2) * (x3*y4 - y3*x4)) / d;
         pt.y = ((x1*y2 - y1*x2) * (y3-y4) - (y1-y2) * (x3*y4 - y3*x4)) / d;
         //-10 is a threshold, the POI can be off by at most 10 pixels
-        if(pt.x<min(x1,x2)-10||pt.x>max(x1,x2)+10||pt.y<min(y1,y2)-10||pt.y>max(y1,y2)+10){
+        if(pt.x<min(x1,x2)-eps||pt.x>max(x1,x2)+eps||pt.y<min(y1,y2)-eps||pt.y>max(y1,y2)+eps){
             return Point2f(-1,-1);
         }
-        if(pt.x<min(x3,x4)-10||pt.x>max(x3,x4)+10||pt.y<min(y3,y4)-10||pt.y>max(y3,y4)+10){
+        if(pt.x<min(x3,x4)-eps||pt.x>max(x3,x4)+eps||pt.y<min(y3,y4)-eps||pt.y>max(y3,y4)+eps){
             return Point2f(-1,-1);
         }
         return pt;
@@ -352,8 +356,12 @@ void FindConnectedComponents(vector<Vec4i>& lines, Mat& img2, vector<vector<cv::
     printf("Foo");
 }
 
-bool comparator(Point2f a,Point2f b){
+bool comparator_x(Point2f a,Point2f b){
     return a.x<b.x;
+}
+
+bool comparator_y(Point2f a,Point2f b){
+    return a.y<b.y;
 }
 
 void sortCorners(std::vector<cv::Point2f>& corners, cv::Point2f center)
@@ -366,8 +374,8 @@ void sortCorners(std::vector<cv::Point2f>& corners, cv::Point2f center)
         else
             bot.push_back(corners[i]);
     }
-    sort(top.begin(),top.end(),comparator);
-    sort(bot.begin(),bot.end(),comparator);
+    sort(top.begin(),top.end(),comparator_x);
+    sort(bot.begin(),bot.end(),comparator_x);
     cv::Point2f tl = top[0];
     cv::Point2f tr = top[top.size()-1];
     cv::Point2f bl = bot[0];
@@ -379,84 +387,185 @@ void sortCorners(std::vector<cv::Point2f>& corners, cv::Point2f center)
     corners.push_back(bl);
 }
 
+
+cv::Point2f RotatePoint(const cv::Point2f& p, float rad)
+{
+    const float x = std::cos(rad) * p.x - std::sin(rad) * p.y;
+    const float y = std::sin(rad) * p.x + std::cos(rad) * p.y;
+    
+    const cv::Point2f rot_p(x, y);
+    return rot_p;
+}
+
+cv::Point2f RotatePoint(const cv::Point2f& cen_pt, const cv::Point2f& p, float rad)
+{
+    const cv::Point2f trans_pt = p - cen_pt;
+    const cv::Point2f rot_pt   = RotatePoint(trans_pt, rad);
+    const cv::Point2f fin_pt   = rot_pt + cen_pt;
+    
+    return fin_pt;
+}
+
+
 int main(int argc, char** argv)
 {
     using namespace cv;
     
-    Mat img;
-    img = imread("check_image_small.jpg");
+    Mat origImg;
+    origImg = imread("check_image.jpg");
     
-    Mat gray;
-    cvtColor(img, gray, COLOR_BGR2GRAY);
-    
-    Mat blur;
-    GaussianBlur(gray, blur, Size(3, 3), 0);
-    
-    Mat edges;
-    Canny(blur, edges, 0, 255);
-    
-    vector<Vec2f> lines;
-    HoughLines(edges, lines, 1, CV_PI/180, 100, 0);
-    
-    Mat lineImage;
-    cvtColor(edges, lineImage, CV_GRAY2BGR);
-    
-    for( size_t i = 0; i < lines.size(); i++ )
-    {
-        float rho = lines[i][0], theta = lines[i][1];
-        Point pt1, pt2;
-        double a = cos(theta), b = sin(theta);
-        double x0 = a*rho, y0 = b*rho;
-        pt1.x = cvRound(x0 + 1000*(-b));
-        pt1.y = cvRound(y0 + 1000*(a));
-        pt2.x = cvRound(x0 - 1000*(-b));
-        pt2.y = cvRound(y0 - 1000*(a));
-        line( lineImage, pt1, pt2, Scalar(0,0,255), 3, CV_AA);
-    }
-    
-    Mat probLineImage;
-    cvtColor(edges, probLineImage, CV_GRAY2BGR);
-    
-    vector<Vec4i> probLines;
-    HoughLinesP(edges, probLines, 1, CV_PI/180, 100, 200, 100 );
-    
-    for( size_t i = 0; i < probLines.size(); i++ )
-    {
-        Vec4i l = probLines[i];
-        line( probLineImage, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(i * 10,i * 20,255), 3, CV_AA);
-    }
-    
+    int pass = 0;
     vector<vector<cv::Point2f>> corners;
+
+    vector<cv::Point2f> rectangleBounds;
+    Mat rot_mat;
+    float angle = 0;
+    cv::Point2f left, right;
     
-    FindConnectedComponents(probLines, edges, corners);
+    Mat img;
+    resize(origImg, img, Size(), 0.25, 0.25, INTER_CUBIC);
     
-    for(int i=0;i<corners.size();i++)
+    while(pass < 2)
     {
-        cv::Point2f center(0,0);
-        if(corners[i].size()<4)continue;
-        for(int j=0;j<corners[i].size();j++){
-            center += corners[i][j];
-        }
-        center *= (1. / corners[i].size());
-        sortCorners(corners[i], center);
-    }
-    
-    
-    for (auto corner : corners)
-    {
-        for (auto point : corner)
+        
+        Mat gray;
+        cvtColor(img, gray, COLOR_BGR2GRAY);
+        
+        
+        Mat blur;
+        GaussianBlur(gray, blur, Size(3, 3), 0);
+        
+        Mat edges;
+        Canny(blur, edges, 0, 255);
+        
+        Mat probLineImage;
+        cvtColor(edges, probLineImage, CV_GRAY2BGR);
+        
+        vector<Vec4i> probLines;
+        HoughLinesP(edges, probLines, 1, CV_PI/180, 100, 200, 100 );
+        
+        for( size_t i = 0; i < probLines.size(); i++ )
         {
-            circle(probLineImage, point, 5, Scalar(0, 0, 255));
+            Vec4i l = probLines[i];
+            line( probLineImage, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(i * 10,i * 20,255), 3, CV_AA);
         }
+        
+        corners.clear();
+        
+        FindConnectedComponents(probLines, edges, corners);
+        
+        for(int i=0;i<corners.size();i++)
+        {
+            cv::Point2f center(0,0);
+            
+            if(corners[i].size()<4)
+            {
+                corners.erase(corners.begin() + i);
+                i--;
+                continue;
+            }
+            
+            for(int j=0;j<corners[i].size();j++){
+                center += corners[i][j];
+            }
+            center *= (1. / corners[i].size());
+            sortCorners(corners[i], center);
+        }
+        
+        
+        for (auto corner : corners)
+        {
+            for (auto point : corner)
+            {
+                circle(probLineImage, point, 50, Scalar(0, 0, 255), 10);
+            }
+        }
+        
+        rectangleBounds = corners[0];
+        
+        sort(rectangleBounds.begin(), rectangleBounds.end(), comparator_y);
+        
+        // Last two elements are the bottom
+        
+        if (rectangleBounds[2].x < rectangleBounds[3].x)
+        {
+            left = rectangleBounds[2];
+            right = rectangleBounds[3];
+            
+            // In this case, right is lower down
+            
+            float opposite = right.y - left.y;
+            float adjacent = right.x - left.x;
+            angle = cvFastArctan(opposite, adjacent);
+        }
+        else
+        {
+            right = rectangleBounds[2];
+            left = rectangleBounds[3];
+            
+            // In this case, left is lower down
+            
+            float opposite = left.y - right.y;
+            float adjacent = right.x - left.x;
+            angle = cvFastArctan(opposite, adjacent);
+            
+            angle *= -1;
+            
+        }
+        
+        rot_mat = getRotationMatrix2D(left, angle, 1.0);
+        Mat dst;
+        warpAffine(img, dst, rot_mat, img.size());
+
+        cv::imshow("blur", blur);
+        cv::imshow("x", edges);
+        cv::imshow("probLines", probLineImage);
+        cv::imshow("rotated", dst);
+        cv::waitKey();
+        
+        dst.copyTo(img);
+        
+        //cv::destroyAllWindows();
+        
+        pass++;
     }
     
     
-    cv::imshow("blur", blur);
-    cv::imshow("x", edges);
-    cv::imshow("lines", lineImage);
-    cv::imshow("probLines", probLineImage);
+    vector<cv::Point2f> rotatedPoints;
+    
+    for (auto point : corners[0])
+    {
+        cv::Point2f rotatedPoint = RotatePoint(left, point, (-angle / 180.0f) * CV_PI);
+        rotatedPoints.push_back(rotatedPoint);
+    }
+    
+    
+    
+    /*
+    Rect r = boundingRect(rotatedPoints);
+
+
+    // Define the destination image
+    cv::Mat quad = cv::Mat::zeros(r.height, r.width, CV_8UC3);
+    // Corners of the destination image
+    std::vector<cv::Point2f> quad_pts;
+    quad_pts.push_back(cv::Point2f(0, 0));
+    quad_pts.push_back(cv::Point2f(quad.cols, 0));
+    quad_pts.push_back(cv::Point2f(quad.cols, quad.rows));
+    quad_pts.push_back(cv::Point2f(0, quad.rows));
+    // Get transformation matrix
+    cv::Mat transmtx = cv::getPerspectiveTransform(rotatedPoints, quad_pts);
+    // Apply perspective transformation
+    cv::warpPerspective(img, quad, transmtx, quad.size());
+    std::stringstream ss;
+    ss<<0<<".jpg";
+    imshow(ss.str(), quad);
+
+    
+    cv::imshow("Final", img);
     cv::waitKey();
     
+    cv::imwrite("extracted_check.jpg", quad);*/
     
     return 0;
 }
